5e26768722fdcb9fe54f5bbb6b6436ab41072848
TimoxaV <vadim2493@gmail.com>
2020-07-29 17:05:48 +0300

    added solution 3.3

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -21,6 +21,8 @@ public class Palindrome {
      * <p>Результат: true</p>
      */
     public boolean isPalindrome(String text) {
-        return false;
+        String deleteSymbols = text.replaceAll("[^\\w\\d]", "").toLowerCase();
+        StringBuilder textReverse = new StringBuilder(deleteSymbols).reverse();
+        return deleteSymbols.equals(textReverse.toString());
     }
 }
